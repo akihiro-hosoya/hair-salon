d7f39b94a54bf997120f870f9288889d1c99d5ed
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-10-11 14:39:40 +0900

    表示

---
+++ accounts/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from allauth.account.forms import SignupForm
+from app.models import User, Staff
+from .models import CustomUser
+
+
+class SignupUserForm(SignupForm):
+    name = forms.CharField(max_length=50, label='名前')
+    furigana = forms.CharField(max_length=50, label='フリガナ')
+    address = forms.CharField(max_length=100, label='住所')
+    tel = forms.CharField(max_length=100, label='電話番号')
+
+    def save(self, request):
+        user = super(SignupUserForm, self).save(request)
+        user_profile = User()
+        user_profile.account_core = CustomUser.objects.filter(email=self.cleaned_data['email'])[0]
+        user_profile.name = self.cleaned_data['name']
+        user_profile.name = self.cleaned_data['furigana']
+        user_profile.address = self.cleaned_data['address']
+        user_profile.address = self.cleaned_data['tel']
+        user_profile.save()
+        return user
+
+class SignupStaffForm(SignupForm):
+    name = forms.CharField(max_length=50, label='名前')
+    furigana = forms.CharField(max_length=50, label='フリガナ')
+    position = forms.CharField(max_length=50, label='役職')
+    address = forms.CharField(max_length=100, label='住所')
+    tel = forms.CharField(max_length=100, label='電話番号')
+
+    def save(self, request):
+        user = super(SignupStaffForm, self).save(request)
+        staff = Staff()
+        staff.account_core = CustomUser.objects.filter(email=self.cleaned_data['email'])[0]
+        staff.name = self.cleaned_data['name']
+        staff.furigana = self.cleaned_data['furigana']
+        staff.position = self.cleaned_data['position']
+        staff.address = self.cleaned_data['address']
+        staff.tel = self.cleaned_data['tel']
+        staff.save()
+        return user
--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from accounts.models import CustomUser
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    account_core = models.OneToOneField(CustomUser, verbose_name='アカウント', on_delete=models.CASCADE)
+    furigana = models.CharField(('フリガナ'), max_length=50, null=True, blank=True)
+    tel = models.CharField('電話番号', max_length=100, null=True, blank=True)
+    adress = models.CharField(('住所'), max_length=100, null=True, blank=True)
+    description = models.TextField('備考欄', default="", blank=True)
+    created = models.DateTimeField(('来店日'), default=timezone.now)
+
+    def __str__(self):
+        return self.account_core.name
+
+class Staff(models.Model):
+    account_core = models.OneToOneField(CustomUser, verbose_name='アカウント', on_delete=models.CASCADE)
+    furigana = models.CharField(('フリガナ'), max_length=50, null=True, blank=True)
+    position = models.CharField(('役職'), max_length=30, blank=True)
+    description = models.TextField('自己紹介', default="", blank=True)
+    image = models.ImageField(upload_to='images', verbose_name='プロフィール画像', null=True, blank=True)
+    tel = models.CharField('電話番号', max_length=100, null=True, blank=True)
+    adress = models.CharField(('住所'), max_length=100, null=True, blank=True)
+    created = models.DateTimeField(('勤務開始日'), default=timezone.now)
+
+    def __str__(self):
+        return self.account_core.name
